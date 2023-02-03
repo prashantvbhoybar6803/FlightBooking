609595c2e775e8bfcd86139b8fdab76735721ed4
prashant <prashant.bhoybar6803@gmail.com>
2023-02-03 23:47:16 +0530

    Only fetching flights 3 hours before departure.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,5 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all.where('departure_time >':Time.now + 3.hours)
+    @flights = Flight.all.where('departure_time > ?',Time.now + 3.hours)
   end
 end
